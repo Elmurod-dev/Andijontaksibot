1ee46ce8b6851cba4ff17ca1eb83b7f794f55f64
ElmurodNarzullayevdevTaTu <elmurodnarzullayev8@gmail.com>
2025-02-08 01:01:31 +0500

    Create Model

--- bot/handlers/__init__.py
+++ bot/handlers/__init__.py
@@ -1,7 +1,9 @@
 from bot.dispacher import dp
 from bot.handlers.main_handler import main_router
+from bot.handlers.driver_handler import driver_router
 
 
 dp.include_routers(*[
-    main_router
+    main_router,
+    driver_router
 ])
